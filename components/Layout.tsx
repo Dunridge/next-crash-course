3302860fb87d71b949c9f11c96da30a310ccede0
Dunridge <Polinka7max@gmail.com>
2023-01-14 19:12:55 -0500

    develop. configure routing

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,17 @@
 import styles from "../src/styles/Layout.module.css";
+import Header from "./Header";
 import Nav from "./Nav";
 
-// TODO: learn the type
+// TODO: learn the type of the layout props
 const Layout = ({ children }: any) => {
   return (
     <>
       <Nav />
       <div className={styles.container}>
-        <main className={styles.main}>{children}</main>
+        <main className={styles.main}>
+            <Header/>
+            {children}
+        </main>
       </div>
     </>
   );
