11da5c0031c93aa4b1ca9425fddb5d97c942cd74
Dunridge <Polinka7max@gmail.com>
2023-01-23 11:41:09 -0800

    develop. add static paths

--- src/pages/article/[id]/index.tsx
+++ src/pages/article/[id]/index.tsx
@@ -1,30 +1,40 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
-const article = (props: any) => {
-    console.log('props', props);
-
-    return (<>
-        <h2>{props?.user}</h2>
-        <p>{props?.fact}</p>
-        <Link href='/'>Go back</Link>
-        </>);
+const article = ({ article }: any) => {
+  return (
+    <>
+      <h2>{article?.title}</h2>
+      <p>{article?.body}</p>
+      <Link href="/">Go back</Link>
+    </>
+  );
 };
 
-export const getServerSideProps = async (context: any) => {
-    // TODO: fix the error here
-    const res = await fetch('https://cat-fact.herokuapp.com/facts');
-    const facts = await res.json();
-    console.log('context', context);
-    const id = Math.floor(Math.random() * 6); 
-    console.log('facts[id]', facts[id]);
+export const getStaticProps = async (context: any) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/${context.params.id}`
+  );
+  const articles = await res.json();
 
-    return {
-        props: {
-            user: facts[id].user ? facts[id].user : 'Undefined',
-            fact: facts[id].text ? facts[id].text : 'Random desc' 
-        }
-    }
-}
+  return {
+    props: articles,
+  };
+};
+
+export const getStaticPaths = async (id: string) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  const ids = articles.map((article: any) => article.id);
+  const paths = ids.map((id: string) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export default article;
